f19e49c713d8448125c5cd67b18c08149f174817
LeeMiRyeong <117430814+ddddddddggod@users.noreply.github.com>
2025-05-15 00:10:00 +0900

    Create RGB_graph.py

---
+++ RGB_graph.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+src = cv2.imread('1.jpg')
+
+if src is None:
+    print('Image load failed!')
+    sys.exit()
+
+colors = ['b', 'g', 'r']
+bgr_planes = cv2.split(src)
+
+for (p, c) in zip(bgr_planes, colors):
+    hist = cv2.calcHist([p], [0], None, [256], [0, 256])
+    plt.plot(hist, color=c)
+
+cv2.imshow('src', src)
+cv2.waitKey(1)
+
+plt.show()
